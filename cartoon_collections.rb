2b9d184d1f0ba424af49a8c5847a2d527c5a0ca4
Donald Lobree <edl1155@gmail.com>
2019-09-26 17:06:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,12 +3,14 @@ def roll_call_dwarves(array)# code an argument here
   array.each do |dwarf|
     puts dwarf
   end
-
-end
 end
 
-def summon_captain_planet# code an argument here
+
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |element|
+    element.capitalize() + "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
