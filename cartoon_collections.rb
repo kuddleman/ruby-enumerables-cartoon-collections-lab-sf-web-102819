8208e7092751c3d8e77bbc9d3fb22662a0d02f7f
Donald Lobree <edl1155@gmail.com>
2019-09-26 17:18:03 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,14 @@ def summon_captain_planet(array)# code an argument here
 end
 
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  array.each do |word|
+    if word.length > 4
+      return true
+    end 
+  end
+  false
 end
 
 def find_the_cheese# code an argument here
